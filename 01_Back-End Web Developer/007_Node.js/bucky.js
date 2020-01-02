e464881cc66290129b2ae020a5ee90d74e11ae18
abdibogor <bogoreh_code@outlook.com>
2020-01-02 16:39:41 +0300

    the first commit

---
+++ 01_Back-End Web Developer/007_Node.js/bucky.js
@@ -0,0 +1,2 @@
+var movies = require('./movies');
+console.log(Emily's favourite movie is: " + movies.favMovie);
---
+++ 01_Back-End Web Developer/007_Node.js/movies.js
@@ -0,0 +1,15 @@
+var movies = require('./movies');
+movies.printavatar();
+console.log(movies.favMovie);
+
+//9. More on Modules
+module.exports = {
+  printAvatart: functions(){
+   console.log("Avatar");
+},
+
+  printchapp: function(){
+    console.log("Avatar");
+},
+    favMovies: "The Matrix"
+}
