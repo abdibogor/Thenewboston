37ed61d6ff3f980b7cb4659a932ab8c7961438eb
abdibogor <bogoreh_code@outlook.com>
2020-01-02 16:42:02 +0300

    the first commit

---
+++ 01_Back-End Web Developer/007_Node.js/012_Core Modules/apps.js
@@ -0,0 +1,25 @@
+/*
+var fs = require('fs');
+fs.writefileSync("corn.txt", "corn is good, corn is life");
+console.log{fs.readFileSync('corn.txt')toString()};
+*/
+
+/*
+var path = require('path');
+var websiteHome = "Desktop/Bucky/thenewboston/index.html
+var websiteAbout = "Desktop/Bucky/thenewboston/index.html
+
+console.log{path.normalize(websiteHome)};
+console.log{path.dirname(websiteAbout)};
+console.log{path.basename(websiteAbout)};
+console.log{path.extname(websiteAbout)};
+*/
+
+/*
+setinterval(function () {
+    console.log("beef");
+},2000);
+*/
+
+console.log(_dirname);
+console.log(_filename);
